36b788308cb96c94f46351d17fd23f0cdab91e91
iemeka <mekstines@gmail.com>
2020-07-20 20:28:37 +0100

    visualizing recursion

---
+++ data structures/recursion.py
@@ -0,0 +1,26 @@
+import turtle
+def tree(branch_len, t,p_size):
+    if branch_len > 5:
+        t.pencolor('brown')
+        t.forward(branch_len)
+        t.pensize(8 - p_size)
+        t.right(20)
+        tree(branch_len - 15, t,p_size+2)
+        t.left(40)
+        t.pensize(8- p_size)
+        tree(branch_len - 15, t,p_size+2)
+        t.pencolor('green')
+        t.right(20)
+        t.backward(branch_len)
+def main():
+    t = turtle.Turtle()
+    my_win = turtle.Screen()
+    t.pencolor('black')
+    t.pensize(9)
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    tree(75, t,0)
+    my_win.exitonclick()
+main()
