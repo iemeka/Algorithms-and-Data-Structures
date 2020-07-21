f8a096bf28de68a543dc3a4c2ffbe5c7dcdc2c1c
iemeka <mekstines@gmail.com>
2020-07-21 21:14:50 +0100

    modifiedrecursion visualization tree with random branches

--- data structures/recursion.py
+++ data structures/recursion.py
@@ -1,14 +1,20 @@
 import turtle
+import random
+
+def randeg():
+    i = random.randint(15,20)
+    yield i
+# 
 def tree(branch_len, t,p_size):
     if branch_len > 5:
         t.pencolor('brown')
         t.forward(branch_len)
         t.pensize(8 - p_size)
         t.right(20)
-        tree(branch_len - 15, t,p_size+2)
+        tree(branch_len-next(randeg()), t,p_size+2)
         t.left(40)
         t.pensize(8- p_size)
-        tree(branch_len - 15, t,p_size+2)
+        tree(branch_len-next(randeg()), t,p_size+2)
         t.pencolor('green')
         t.right(20)
         t.backward(branch_len)
@@ -24,3 +30,5 @@ def main():
     tree(75, t,0)
     my_win.exitonclick()
 main()
+
+
